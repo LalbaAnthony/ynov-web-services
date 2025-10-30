e8fccc5a05618112ecaa11c0f23d271b0b474c5f
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-10-30 09:01:14 +0100

    Add some book + , tood

--- data/booksDB.js
+++ data/booksDB.js
@@ -1,6 +1,12 @@
 let mockBookDB = [
   { id: 1, title: "Le Seigneur des Anneaux", author: "J.R.R. Tolkien" },
   { id: 2, title: "Dune", author: "Frank Herbert" },
+  { id: 3, title: "1984", author: "George Orwell" },
+  { id: 4, title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { id: 5, title: "Brave New World", author: "Aldous Huxley" },
+  { id: 6, title: "The Catcher in the Rye", author: "J.D. Salinger" },
+  { id: 7, title: "To Kill a Mockingbird", author: "Harper Lee" },
+  { id: 8, title: "The Great Gatsby", author: "F. Scott Fitzgerald" }
 ];
 
 module.exports = mockBookDB;
